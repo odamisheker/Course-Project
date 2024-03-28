70250fb802e35a5a094ab22e042ecf41d6e4211a
nekit <garkusha500@gmail.com>
2024-03-28 22:18:00 +0300

    add logic for chats

---
+++ client/src/DB.js
@@ -0,0 +1,41 @@
+const chats = [
+  {
+    id: "Даник",
+    userId: "egor",
+    messages: [
+      {
+        text: "hello",
+        time: Date.now(),
+        username: "Igor",
+      },
+    ],
+  },
+  {
+    id: "Далв",
+    userId: "egor",
+    messages: [
+      {
+        text: "Как там с Яной?)",
+        time: Date.now(),
+        username: "Dalv",
+      },
+      {
+        text: "Dai poparit",
+        time: Date.now(),
+        username: "Dalv",
+      },
+    ],
+  },
+  {
+    id: "Некит",
+    userId: "egor",
+    messages: [],
+  },
+  {
+    id: "4",
+    userId: "dalv",
+    messages: [],
+  },
+];
+
+export default chats;
--- client/src/components/WindowChat/WindowChat.jsx
+++ client/src/components/WindowChat/WindowChat.jsx
@@ -1,25 +1,33 @@
-import { useCallback, useContext, useState } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
 import { UserContext } from "../context/UserContextProvider";
 import Message from "../Message/Message";
 import styles from "./WindowChat.module.css";
 import InputMessage from "../InputMessage/InputMessage";
 import ChatToolBar from "../ChatToolBar/ChatToolBar";
 import { ChatContext } from "../context/ChatContextProvider";
+import chats from "../../DB";
 
 export default function WindowChat() {
+  const { id } = useContext(UserContext);
+  const { chatId } = useContext(ChatContext);
   // const [errors, setErrors] = useState(null);
   const [messages, setMessages] = useState([]);
 
   const [message, setMessage] = useState({ text: "" });
+
+  useEffect(() => {
+    if (chatId != null) {
+      setMessages(chats.find((k) => k.id == chatId).messages);
+    }
+  }, [chatId]);
+
+  //console.log(messages);
 
   /*
   TODO: load messanges 
         props(id собеседника или же сразу сообщения)
         хранить сообщение как объект(для хранения текста, времени и тд)
   */
-
-  const { id } = useContext(UserContext);
-  const { chatId } = useContext(ChatContext);
 
   const handleEdit = (data) => {
     setMessage(data);
